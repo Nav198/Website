2f0fb23e6278e067b93d1139dadcb25b45b1d0e2
Nav198 <89772752+Nav198@users.noreply.github.com>
2021-08-30 17:03:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function register()
+{
+	var fname=document.getElementById("fname").value;
+	var lname=document.getElementById("lname").value;
+	var mid=document.getElementById("mid").value;
+	var m=document.getElementById("m").value;
+	var f=document.getElementById("f").value;
+	var mno=document.getElementById("mno").value;
+	var ssc=document.getElementById("ssc").value;
+	var hsc=document.getElementById("hsc").value;
+	var degree=document.getElementById("degree").value;
+    var pg=document.getElementById("pg").value;
+    var fname1=document.getElementById("fathername").value;
+    var mname=document.getElementById("mname").value;
+    var hno=document.getElementById("hno").value;
+    var locality=document.getElementById("locality").value;
+    var landmark=document.getElementById("landmark").value;
+    var pincode=document.getElementById("pincode").value;
+
+
+    document.write(fname);
+document.write(lname);
+document.write(mid);
+document.write(m);
+document.write(f);
+document.write(mno);
+document.write(ssc);
+document.write(hsc);
+document.write(degree);
+document.write(pg);
+document.write(fname1);
+document.write(mname);
+document.write(hno);
+document.write(locality);
+document.write(landmark);
+document.write(pincode);
+
+	
+}
+
+register();
+
+
+
+
